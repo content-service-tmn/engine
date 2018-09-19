3a5729ffb2bbc85d325cad6492068abdf7c659e9
Vova <nikon72ru@gmail.om>
2018-09-19 11:19:06 +0300

    callibri

--- site/templates/common/_done.php
+++ site/templates/common/_done.php
@@ -86,7 +86,7 @@
 
 
 <!— Yandex.Metrika counter —> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter40623600 = new Ya.Metrika({ id:40623600, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/40623600" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!— /Yandex.Metrika counter —>
-
+<script src="//cdn.callibri.ru/callibri.js" type="text/javascript" charset="utf-8"></script>
 
 </body>
 
